97a87b3e0b5778007eb9e5aa5619197767e2ac8a
Felipe Rocha <felipeo.rocha@hotmail.com>
2020-02-26 03:36:09 -0300

    Users: user data input validation

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,7 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { Form, Input } from "@rocketseat/unform";
+import * as Yup from "yup";
 import logo from "~/assets/mask_gas.svg";
+
+const schema = Yup.object().shape({
+  email: Yup.string()
+    .email("Enter a valid email")
+    .required("You must enter your email"),
+  password: Yup.string().required("You must enter the password")
+});
 
 const SignIn = () => {
   const handleSubmit = data => {
@@ -11,7 +19,7 @@ const SignIn = () => {
   return (
     <>
       <img src={logo} alt="tattoo" />
-      <Form onSubmit={handleSubmit}>
+      <Form schema={schema} onSubmit={handleSubmit}>
         <Input name="email" type="email" placeholder="Enter email..." />
         <Input name="password" type="password" placeholder="Enter password" />
         <button type="submit">SignIn</button>
--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,7 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { Form, Input } from "@rocketseat/unform";
+import * as Yup from "yup";
 import logo from "~/assets/mask_gas.svg";
+
+const schema = Yup.object().shape({
+  name: Yup.string().required("You must enter yout user name"),
+  email: Yup.string()
+    .email("Enter a valid email")
+    .required("You must enter your email"),
+  password: Yup.string()
+    .min(6, "You must enter at least 6 char password")
+    .required("You must enter the password")
+});
 
 const SignUp = () => {
   const handleSubmit = data => {
@@ -11,7 +22,7 @@ const SignUp = () => {
   return (
     <>
       <img src={logo} alt="tattoo" />
-      <Form onSubmit={handleSubmit}>
+      <Form schema={schema} onSubmit={handleSubmit}>
         <Input name="name" placeholder="Enter full name" />
         <Input name="email" type="email" placeholder="Enter email..." />
         <Input name="password" type="password" placeholder="Enter password" />
