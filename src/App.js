d41b9a8e06da4790ca18cb0bac2515b7f18b4025
Felipe Rocha <felipeo.rocha@hotmail.com>
2020-02-26 04:02:00 -0300

    Users: redux config

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React from "react";
+import { Provider } from "react-redux";
 import { Router } from "react-router-dom";
 import "./config/ReactotronConfig";
 import Routes from "./routes";
 import history from "./services/history";
+import store from "./store";
 import GlobalStyle from "./styles/global";
 
 const App = () => {
   return (
-    <Router history={history}>
-      <Routes />
-      <GlobalStyle />
-    </Router>
+    <Provider store={store}>
+      <Router history={history}>
+        <Routes />
+        <GlobalStyle />
+      </Router>
+    </Provider>
   );
 };
 
