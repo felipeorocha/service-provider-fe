3bb44b5733241500c63c0800c7435bd29e5e9955
Felipe Rocha <felipeo.rocha@hotmail.com>
2020-02-26 22:07:05 -0300

    Dashboard: dashboard component, subcomponents and navigation implemented

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,9 +1,46 @@
-import React from "react";
+import React, { useState, useMemo } from "react";
+import { format, subDays, addDays } from "date-fns";
+import pt from "date-fns/locale/pt";
+import { MdChevronLeft, MdChevronRight } from "react-icons/md";
 import api from "~/services/api";
 
+import { Container, Time } from "./styles";
+
 const DashBoard = () => {
-  api.get("appointments");
-  return <div>Dashboard</div>;
+  const [date, setDate] = useState(new Date());
+
+  const dateFormatted = useMemo(
+    () => format(date, "d 'de' MMMM", { locale: pt }),
+    [date]
+  );
+
+  const handlePrevDay = () => {
+    setDate(subDays(date, 1));
+  };
+
+  const handleNextDay = () => {
+    setDate(addDays(date, 1));
+  };
+
+  return (
+    <Container>
+      <header>
+        <button type="button" onClick={handlePrevDay}>
+          <MdChevronLeft size={36} color="#fff" />
+        </button>
+        <strong>{dateFormatted}</strong>
+        <button type="button" onClick={handleNextDay}>
+          <MdChevronRight size={36} color="#fff" />
+        </button>
+      </header>
+      <ul>
+        <Time available past>
+          <strong>hour</strong>
+          <span>Name - schedule</span>
+        </Time>
+      </ul>
+    </Container>
+  );
 };
 
 export default DashBoard;
